1451c45e404ec4d7745041e95edcf6a904f27288
Neilansh Rajpal <neilanshjr@gmail.com>
2023-05-13 16:21:07 -0700

    created skeleton for Skills and Projects page

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,10 +1,35 @@
 // Component for the "Projects" section of the website
 
-import React from 'react'
+import React from 'react';
+
+import { FaLaptopCode } from 'react-icons/fa';
 
 const Projects = () => {
   return (
-    <section id='projects'>Projects</section>
+    <section id='projects'>
+      <div className='mx-auto px-10 py-20 container flex flex-col items-center  md:flex-row '>
+        <div className='mb-16 flex flex-col items-center text-left sm:text-center  md:pr-16  md:mb-0 md:w-1/2  lg:flex-grow lg:pr-24'>
+          <FaLaptopCode size={50} color='white' className='mb-4'/>
+          <h1 className='mb-4 title-font font-medium text-white text-4xl'>
+          Projects
+          </h1>
+          <p className="mb-8 text-lg leading-relaxed">
+          I have had the opportunity to apply my skills to projects. While I had fun collaborating with my peers on academic projects, 
+          I also learned a lot by working on individual side projects.
+        </p>
+        </div>
+
+
+
+
+
+</div>
+    
+    
+    
+    
+    
+    </section>
   )
 }
 
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,22 +2,19 @@
 
 import React from 'react'
 
-import { RiCodeSSlashFill, RiCodeSSlashLine } from 'react-icons/ri';
+import { RiCodeSSlashFill } from 'react-icons/ri';
 
 const Skills = () => {
   return (
     <section id='skills'> 
       <div className='mx-auto px-10 py-20 container flex flex-col items-center  md:flex-row '>
-        <div className='mb-16 flex flex-col items-center text-center  md:pr-16 md:text-left md:mb-0 md:w-1/2  lg:flex-grow lg:pr-24'>
+        <div className='mb-16 flex flex-col items-center text-left  sm:text-center  md:pr-16  md:mb-0 md:w-1/2  lg:flex-grow lg:pr-24'>
           <RiCodeSSlashFill size={50} color='white' className='mb-4'/>
           <h1 className='mb-4 title-font font-medium text-white text-4xl'>
           Skills
           </h1>
           <p className="mb-8 text-lg leading-relaxed">
-          Originally from Ahmedabad, India, I'm a Computer Science graduate at UBC Okanagan, Canada. This is my portfolio website. 
-          Follow along to know more about me! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo
-          facilis repellat ab cupiditate alias vero aliquid obcaecati quisquam
-          fuga dolore.
+         Through the years, I have been exposed to different tools and frameworks, and have been able to add a few skills up my arsenal.
           </p>
         </div>
 
