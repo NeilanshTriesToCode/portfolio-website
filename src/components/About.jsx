d47ba6db283952ae08c6795473f401ba7da927ab
Neilansh Rajpal <neilanshjr@gmail.com>
2023-05-13 12:26:32 -0700

    finished up About page; started Hobbies page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,11 +3,25 @@
 import React from 'react'
 
 const About = () => {
+
   return (
     <section id='about'>
-        <div className='container mx-auto flex px-10 py-20 md:flex-row flex-col items-center'>
-            <h1>Hola, <br /> I'm Neilansh</h1>
-    
+        <div className='mx-auto px-10 py-20 container flex flex-col items-center  md:flex-row '>
+          <div className='mb-16 flex flex-col items-center text-center  md:pr-16 md:items-start md:text-left md:mb-0 md:w-1/2  lg:flex-grow lg:pr-24'>
+            <h1 className='mb-4 title-font font-medium text-white text-4xl md:text-5xl'>
+              Hola, <br /> I'm Neilansh
+            </h1>
+            <p className="mb-8 text-xl leading-relaxed">
+            Originally from Ahmedabad, India, I'm a Computer Science graduate at UBC Okanagan, Canada. This is my portfolio website. 
+            Follow along to know more about me! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo
+            facilis repellat ab cupiditate alias vero aliquid obcaecati quisquam
+            fuga dolore.
+            </p>
+          </div>
+
+          <div className='w-5/6 flex justify-center  md:w-1/2  lg:max-w-lg lg:w-full'>
+            <img src='profile.jpg' alt='profile_img' className=' h-96 w-80 rounded-full ring-2 ring-slate-600'/>
+          </div>
     
         </div>
     </section>
--- src/components/Hobbies.jsx
+++ src/components/Hobbies.jsx
@@ -4,7 +4,21 @@ import React from 'react'
 
 const Hobbies = () => {
   return (
-    <section id='hobbies'>Hobbies</section>
+    <section id='hobbies'>
+      <div className='mx-auto px-10 py-20 container flex flex-col items-center  md:flex-row '>
+        <div className='mb-16 flex flex-col items-center text-center  md:pr-16 md:text-left md:mb-0 md:w-1/2  lg:flex-grow lg:pr-24'>
+          <h1 className='mb-4 title-font font-medium text-white text-4xl'>
+            Interests & Hobbies
+          </h1>
+          
+        </div>
+    
+    
+    
+    
+    
+      </div>
+    </section>
   )
 }
 
