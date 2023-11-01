49613a249e603c721b35733c794bc839306792c4
Neilansh Rajpal <neilanshjr@gmail.com>
2023-11-01 18:16:02 +0530

    started work on #7 - will create a new branch

---
+++ src/components/TypeWriter.jsx
@@ -0,0 +1,10 @@
+// Component displaying text, with a Typing effect
+import React, { useState, useEffect, useRef } from 'react';
+
+const TypeWriter = ({text}) => {
+  return (
+    <div>TypeWriter</div>
+  )
+}
+
+export default TypeWriter;
